f9c77a84afeeb3f9d28e82e8bce0aee17d75b3de
Ismat-Samadov <ismetsemedov@gmail.com>
2023-12-20 19:36:40 +0400

    check

--- Dicee Challenge - Starting Files/script.js
+++ Dicee Challenge - Starting Files/script.js
@@ -3,11 +3,9 @@ function playGame() {
     var player2Dice = document.getElementById('player2Dice');
     var resultElement = document.getElementById('result');
 
-    // Roll dice for each player
     var player1Roll = rollDice(player1Dice);
     var player2Roll = rollDice(player2Dice);
 
-    // Display the result
     var result = determineWinner(player1Roll, player2Roll);
     resultElement.textContent = result;
 }
@@ -15,7 +13,6 @@ function playGame() {
 function rollDice(diceElement) {
     var randomNumber = Math.floor(Math.random() * 6) + 1;
 
-    // You can customize the dice faces as needed
     var diceFaces = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
 
     diceElement.textContent = diceFaces[randomNumber - 1];
