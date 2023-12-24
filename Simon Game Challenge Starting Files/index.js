c32f6de11dc9180b16c031e3688d9e91425f3872
Ismat-Samadov <ismetsemedov@gmail.com>
2023-12-24 11:26:59 +0400

    check

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -4,24 +4,23 @@ var gamePattern = [];
 var userChosenColour = "";
 var userClickedPattern = [];
 var level = 0;
+var gameStarted = false;
 
 
+$(document).keydown(clickDetector);
+$(".btn").click(userChoice);
 
-$(".btn").click(userChoice());
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
 
-function nextSequence() { 
   var randomNumber = Math.floor(Math.random() * 3);
   var randomChosenColour = buttonColours[randomNumber];
   flash(randomChosenColour);
   playSound(randomChosenColour);
 }
 
-function userChoice() {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-}
+
 
 function playSound(name_of_sound){ 
   var audio = new Audio("sounds/" + name_of_sound + ".mp3");
@@ -38,4 +37,20 @@ function animatePress(currentColour) {
   setTimeout(() => {
     $("#"+currentColour).removeClass('pressed');
   }, 100);
+}
+
+
+function userChoice() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+}
+
+function clickDetector() {
+  if (!gameStarted) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    gameStarted = true;
+  }
 }
