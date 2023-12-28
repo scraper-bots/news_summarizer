1994ae40f3a6186dd24561da32ee6014e2920ed7
Ismat-Samadov <ismetsemedov@gmail.com>
2023-12-28 15:41:53 +0400

    check

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,24 +1,19 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
-
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = 3000;
 const password = 'ILoveProgramming';
+var userAuthenticated = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function checkPassword(req, res, next) {
   if (req.password === password) { 
-    return true;
+    userAuthenticated= true;
   }
   next();
 }
@@ -32,7 +27,13 @@ app.get("/", (req, res) => {
 
 
 app.post("/submit", (req, res) => {
-  res.sendFile(__dirname + "/public/secret.html");
+  if (userAuthenticated) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else { 
+      res.sendFile(__dirname + "/public/index.html");
+
+  }
+
 });
 
 app.listen(port, () => {
