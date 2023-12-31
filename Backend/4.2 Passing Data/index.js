000afbbdc88b038e66b6ca56c681a33d99c1b7ed
Ismat-Samadov <ismetsemedov@gmail.com>
2023-12-31 16:36:46 +0400

    check

--- Backend/4.2 Passing Data/index.js
+++ Backend/4.2 Passing Data/index.js
@@ -6,23 +6,9 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Middleware to calculate and log the length of the names
-function getNameLength(req, res, next) {
-  let fName = req.body['fName'];
-  let lName = req.body['lName'];
-  let lengths = fName.length + lName.length;
-  console.log(`Total length of names: ${lengths}`);
-  next(); // Call next to move to the next middleware
-}
+app.get("/", (req, res) => {});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/submit", getNameLength, (req, res) => {
-
-  res.send("Form submitted successfully!");
-});
+app.post("/submit", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
