48b2b57ddd77b18f062bb2e59ef3d80f4f7a2f6c
Ismat-Samadov <ismetsemedov@gmail.com>
2024-01-03 12:53:16 +0400

    check

--- Backend/4.3 EJS Partials/index.js
+++ Backend/4.3 EJS Partials/index.js
@@ -10,6 +10,11 @@ Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
